43989557eb03e42cd3898f18059f57136b9f60de
AhmedRaza186 <sallumoon650@gmail.com>
2025-10-16 23:38:25 +0500

    ...

--- app.js
+++ app.js
@@ -8,6 +8,31 @@ function openCart() {
 function closeCart() {
   const sidebar = document.getElementById('cart-sidebar')
   sidebar.style.right = '-100%'
-    const overlay = document.getElementById('overlay')
-    setTimeout(() => overlay.style.display = 'none', 200)
+  const overlay = document.getElementById('overlay')
+  setTimeout(() => overlay.style.display = 'none', 200)
+}
+
+function changeToMen() {
+  document.getElementById('women-container').style.display = 'none'
+  document.getElementById('men-container').style.display = 'grid'
+  document.getElementById('men').style.color = '#212121'
+  document.getElementById('women').style.color = '#757575'
+
+
+}
+function changeToWomen() {
+  document.getElementById('men-container').style.display = 'none'
+  document.getElementById('women-container').style.display = 'grid'
+  document.getElementById('women').style.color = '#212121'
+  document.getElementById('men').style.color = '#757575'
+}
+
+function frontPic(img) {
+  const idNum = img.id.replace('dress', '')
+  img.src = `./dress${idNum}.png`
+}
+
+function backPic(img) {
+  const idNum = img.id.replace('dress', '')
+  img.src = `./back${idNum}.png`
 }
