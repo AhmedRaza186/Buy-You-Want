249d98f43ff30fbfefb7f97daba5dcf3abdad658
AhmedRaza186 <sallumoon650@gmail.com>
2025-10-18 00:57:22 +0500

    add to cart added

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ function changeToWomen() {
   document.getElementById('men').style.color = '#757575'
 }
 
+
 function frontPic(img) {
   const idNum = img.id.replace('dress', '')
   img.src = `./dress${idNum}.png`
@@ -36,10 +37,40 @@ function backPic(img) {
   const idNum = img.id.replace('dress', '')
   img.src = `./back${idNum}.png`
 }
+let subArr = []
 
 function addToCart(btn) {
-  // Find the parent item card
   let item = btn.closest('.items');
-  
+  let img = item.firstElementChild.src
+  let title = item.children[1].innerText
+  let description = item.children[2].innerText
+  let price = item.children[3].firstElementChild.innerText
 
+  let sidebarItem = document.createElement('div')
+  sidebarItem.setAttribute('class', 'sidebar-item')
+  sidebarItem.innerHTML = `<img src="${img}" alt=""><div class="sidebar-item-txt"><h3>${title}</h3><p>${description}</p></div><div class="sidebar-price-dlt"><p>${price}</p><button onclick="removeFromCart(this)">Remove From Cart</button></div>`
+  document.getElementsByClassName('sidebar-main')[0].appendChild(sidebarItem)
+  priceNum = Number(price.replace('$', ''))
+  subArr.push(priceNum)
+  let subTotal = subArr.reduce(function(total,curr){
+   return total + curr
+
+  },0)
+  subTotal = subTotal.toFixed(2)
+
+subTotal = Number(subTotal)
+let tax = Number( subTotal / 7)
+let total = Number( subTotal + tax)
+
+tax = Math.floor(tax)
+total = total.toFixed(2)
+document.getElementById('subtotal').lastElementChild.innerText = `$${subTotal}`
+document.getElementById('total').lastElementChild.innerText = `$${tax}`
+document.getElementById('total').lastElementChild.innerText = `$${total}`
+console.log(tax)
+  
+}
+
+function removeFromCart(btn){
+btn.closest('.sidebar-item').style.display = 'none'
 }
